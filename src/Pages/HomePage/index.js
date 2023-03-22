0ff08e340b804ae6c501ce6173f99e2fda8557f0
sophiayu86 <sophiayu86@gmail.com>
2023-03-22 23:42:20 +0800

    fix:dependency

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -5,7 +5,7 @@ const HomePage = () => {
   const navigate = useNavigate();
   useEffect(() => {
     navigate("/login");
-  }, []);
+  }, [navigate]);
   return <div>HomePage redirect to login or main</div>;
 };
 export default HomePage;
