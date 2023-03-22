65daa6fb2e319b02e7eac1fb8ae0335ef8072d49
sophiayu86 <sophiayu86@gmail.com>
2023-03-22 23:36:25 +0800

    chore: direct demo

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect } from "react";
 // import styles from './style.module.css';
-
-const HomePage=()=>{
-    return(
-    <div>
-        HomePage redirect to login or main
-    </div>
-    )
-}
+import { useNavigate } from "react-router-dom";
+const HomePage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/login");
+  }, []);
+  return <div>HomePage redirect to login or main</div>;
+};
 export default HomePage;
