bc102996583305574b5503adffb5282338bacb3a
sophiayu86 <sophiayu86@gmail.com>
2023-03-24 01:04:31 +0800

    feat: popularList & popularItem

--- src/Components/index.js
+++ src/Components/index.js
@@ -4,3 +4,4 @@ export { default as LoginInput } from './LoginInput';
 export { default as SideNav } from './SideNav';
 export { default as Header } from './Header';
 export { default as ProfileHeader } from './ProfileHeader';
+export { default as PopularItem } from './PopularItem';
