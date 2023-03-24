5651801c9606f96687723c0e7ca4b6d193d2717d
evojroan <roan6903@gmail.com>
2023-03-24 12:30:45 +0800

    Feat: Adds UserCard; Fix: SideNav has two types of icons according to currentPage.

--- src/Components/UserCard/index.js
+++ src/Components/UserCard/index.js
@@ -1,24 +1,54 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { ReactComponent as Noti0 } from "../../Assets/icon/noti0.svg";
+import { ReactComponent as Noti1 } from "../../Assets/icon/noti1.svg";
+import { ReactComponent as Msg } from "../../Assets/icon/msg.svg";
 import styles from "./style.module.css";
 
-const UserCard = () => {
+const UserCard = ({
+  username,
+  account,
+  intro,
+  following,
+  follower,
+  isMe,
+  noti,
+}) => {
   return (
-    <div className={styles.UserCard}>
-      <div className={styles.bgImg}></div>
-      <div className={styles.avatar}></div>
+    <div className={styles.UserInfo}>
+      <div className={styles.bgImg}>背景</div>
+      <div className={styles.avatar}>大頭暫時黑框</div>
+
       <div className={styles.userActions}>
-        {isMe? <button>編輯個人資料</button>:3/24 這裡要放 icon}
+        {isMe ? (
+          <button className={styles.editMyProfile}>編輯個人資料</button>
+        ) : (
+          <section>
+            <Msg className={styles.Msg} />
+            {noti ? (
+              <Noti1 className={styles.Noti1} />
+            ) : (
+              <Noti0 className={styles.Noti0} />
+            )}
+            <div className={styles.followButton}>正在跟隨</div>
+          </section>
+        )}
       </div>
-      <div className={styles.username}>Jane Cathy</div>
-      <div className={styles.account}>iamjane1999</div>
-      <div className={styles.description}>Hello this is a pen how are you</div>
+      <div className={styles.nameandaccount}>
+        <span className={styles.username}>
+          {username}
+          Jane Cathy
+        </span>
+        <span className={styles.account}>{account}@cath1999</span>
+      </div>
+
+      <div className={styles.intro}>
+        {intro}"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      </div>
       <div className={styles.followship}>
-        <span className={styles.following}>231 個跟隨中</span>
-        <span className={styles.follower}>45位跟隨者</span>
+        <span className={styles.following}>{following} 個跟隨中</span>
+        <span className={styles.follower}>{follower} 位跟隨者</span>
       </div>
-      <div className={styles.infoZone}></div>
     </div>
   );
 };
--- src/Pages/MainPage/index.js
+++ src/Pages/MainPage/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Header, SideNav } from '../../Components';
-import styles from './style.module.css';
+import React from "react";
+import { Header, SideNav, UserCard } from "../../Components";
+import styles from "./style.module.css";
 
-
-const MainPage=()=>{
-    return(
+const MainPage = () => {
+  return (
     <div className={styles.mainpage}>
-        <SideNav />
+      <SideNav />
+      <div className={styles.middle}>
         <Header text="首頁" />
+        <UserCard />
+      </div>
     </div>
-    )
-}
+  );
+};
 export default MainPage;
