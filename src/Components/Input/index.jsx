c57cfbb81fde15ac1bf70f729b307ab3f926847f
yichennn <yichennn1010@gmail.com>
2023-03-25 03:44:00 +0800

    feat: add Input component

---
+++ src/Components/Input/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useState } from 'react';
+import styles from './style.module.css';
+
+const Input = ({ label, name, type, placeholder, onHandlers, info }) => {
+  const { status, message } = info;
+  const { handleOnChange, handleOnFocus, handleOnBlur } = onHandlers;
+  const [inputValue, setInputValue] = useState('');
+  const inputStyle = status === 'focus' ? styles.inputFocus : status === 'error' ? styles.inputError : styles.inputDefault;
+  return (
+    <div className={styles.inputField}>
+      <div className={styles.inputContainer}>
+        <label className={styles.label}>{label}</label>
+        <input
+          className={inputStyle}
+          type={type || 'text'}
+          value={inputValue || ''}
+          placeholder={placeholder}
+          onChange={e => {
+            setInputValue(e.target.value);
+            handleOnChange?.(name, e.target.value);
+          }}
+          onFocus={e => handleOnFocus?.(name, e.target.value)}
+          onBlur={e => handleOnBlur?.(name, e.target.value)}
+        />
+      </div>
+      <p className={status === 'focus' ? styles.focusMessage : styles.errorMessage}>{message}</p>
+    </div>
+  );
+};
+export default Input;
