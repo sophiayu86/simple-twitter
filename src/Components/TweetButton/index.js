2646fcf0db145881c064a0e0db8f484c5a8e9ede
sophiayu86 <sophiayu86@gmail.com>
2023-03-26 01:17:47 +0800

    feat: fix css for sidebar

--- src/Components/TweetButton/index.js
+++ src/Components/TweetButton/index.js
@@ -3,7 +3,7 @@ import styles from "./style.module.css";
 
 const TweetButton = ({ text }) => {
   return (
-    <span className={styles.TweetButton}>{text}</span>
+    <div className={styles.TweetButton}>{text}</div>
   );
 };
 export default TweetButton;
