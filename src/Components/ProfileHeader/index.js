8d9b337ca0ee3d95884f243d1445a1dd944e0ed6
sophiayu86 <sophiayu86@gmail.com>
2023-03-27 00:33:28 +0800

    fix: headers' navigate back previous page

--- src/Components/ProfileHeader/index.js
+++ src/Components/ProfileHeader/index.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ReactComponent as Back } from "../../Assets/icon/back.svg";
 import styles from "./style.module.css";
 
 const ProfileHeader = ({ text, num }) => {
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate(-1);
+  };
   return (
     <div className={styles.profileheader}>
-      <Link to="/main">
-        <Back className={styles.icon} />
-      </Link>
+      <Back className={styles.icon} onClick={handleBack} />
       <div>
         <div className={styles.headerTitle}>{text}</div>
         <div className={styles.tweetNums}>{num}推文</div>
