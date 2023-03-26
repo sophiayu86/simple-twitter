da2026e95d2d57ab2f01bdd858a899b768770b1a
yichennn <yichennn1010@gmail.com>
2023-03-26 15:16:51 +0800

    feat: user edit modal

--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -2,30 +2,48 @@ import React from 'react';
 import { useState } from 'react';
 import styles from './style.module.css';
 
-const Input = ({ label, name, type, placeholder, status, message, onHandlers }) => {
+const Input = ({ label, name, type, placeholder, status, message, value, onHandlers }) => {
   const handleOnChange = onHandlers?.handleOnChange;
   const handleOnFocus = onHandlers?.handleOnFocus;
   const handleOnBlur = onHandlers?.handleOnBlur;
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState(value || '');
   const inputStyle = status === 'focus' ? styles.inputFocus : status === 'error' ? styles.inputError : styles.inputDefault;
   return (
     <div className={styles.inputField}>
       <div className={styles.inputContainer}>
-        <label className={styles.label}>{label}</label>
-        <input
-          className={inputStyle}
-          type={type || 'text'}
-          value={inputValue || ''}
-          placeholder={placeholder}
-          onChange={e => {
-            setInputValue(e.target.value);
-            handleOnChange?.(name, e.target.value);
-          }}
-          onFocus={e => handleOnFocus?.(name, e.target.value)}
-          onBlur={e => handleOnBlur?.(name, e.target.value)}
-        />
+        <label className={styles.label} htmlFor={name}>
+          {label}
+        </label>
+        {type === 'textarea' ? (
+          <textarea
+            name={name}
+            id={name}
+            rows='4'
+            className={inputStyle}
+            value={inputValue}
+            onChange={e => {
+              setInputValue(e.target.value);
+              handleOnChange?.(name, e.target.value);
+            }}
+            onFocus={e => handleOnFocus?.(name, e.target.value)}
+            onBlur={e => handleOnBlur?.(name, e.target.value)}
+          />
+        ) : (
+          <input
+            className={inputStyle}
+            type={type || 'text'}
+            value={inputValue}
+            placeholder={placeholder}
+            onChange={e => {
+              setInputValue(e.target.value);
+              handleOnChange?.(name, e.target.value);
+            }}
+            onFocus={e => handleOnFocus?.(name, e.target.value)}
+            onBlur={e => handleOnBlur?.(name, e.target.value)}
+          />
+        )}
       </div>
-      <p className={status === 'focus' ? styles.focusMessage : styles.errorMessage}>{message}</p>
+      <p className={status === 'error' ? styles.errorMessage : styles.focusMessage}>{message}</p>
     </div>
   );
 };
---
+++ src/Components/UserEditModal/ModelContent/index.jsx
@@ -0,0 +1,132 @@
+import style from './style.module.css';
+import { Input, NotificationCard } from '../../../Components/index.js';
+import { ReactComponent as Cross } from '../../../Assets/icon/cross.svg';
+import { ReactComponent as CoverEdit } from '../../../Assets/icon/coverEdit.svg';
+import { ReactComponent as AvatarEdit } from '../../../Assets/icon/avatarEdit.svg';
+import { useState } from 'react';
+import { editUserProfile } from '../../../API/editUserProfile.js';
+
+export default function ModalContent({ userData, onClose }) {
+  const { id, name, introduction, avatar, cover } = userData;
+  const initialInputInfo = {
+    name: { status: 'default', message: `${name?.length}/50`, value: name },
+    introduction: { status: 'default', message: `${introduction?.length}/160`, value: introduction }
+  };
+  const [inputInfo, setInputInfo] = useState(initialInputInfo);
+  const [imagePrev, setImagePrev] = useState({ avatar, cover });
+  const [imageFile, setImageFile] = useState({ avatar: '', cover: '' });
+  const [updateResult, setUpdateResult] = useState(null);
+
+  const handleSubmit = async (id, e) => {
+    e.stopPropagation();
+    const { name, introduction } = inputInfo;
+    if (!name.value?.trim()) return setInputInfo(prev => ({ ...prev, name: { status: 'error', message: '名稱不可為空白' } }));
+    if (name.status === 'error' || introduction.status === 'error') return;
+    const formData = new FormData();
+    formData.append('name', inputInfo.name.value);
+    formData.append('introduction', inputInfo.introduction.value);
+    formData.append('cover', imageFile.cover);
+    formData.append('avatar', imageFile.avatar);
+    const result = await editUserProfile({ id, formData }); //回傳值：{status: 'success', message: '檔案更新成功'}
+
+    if (result) {
+      setUpdateResult(result);
+      if (result.status === 'success') {
+        setTimeout(() => onClose(), 1500);
+      } else {
+      }
+    }
+  };
+  const handleClose = e => {
+    e.stopPropagation();
+    onClose();
+  };
+  const handleImageChange = (e, setPrev, setFile) => {
+    const { name } = e.target;
+    const file = e.target.files?.[0]; // 取得選中檔案們的一個檔案
+    const src = file ? window?.URL.createObjectURL(file) : imagePrev[name];
+    setPrev(prev => ({ ...prev, [name]: src }));
+    setFile(prev => ({ ...prev, [name]: file }));
+  };
+  const onHandlers = {
+    handleOnChange: (inputName, value) => {
+      setInputInfo(prev => {
+        if (inputName === 'name') {
+          return value.length > 50
+            ? { ...prev, [inputName]: { status: 'error', message: '不可超過50字', value } }
+            : { ...prev, [inputName]: { status: 'focus', message: `${value?.length}/50`, value } };
+        }
+        if (inputName === 'introduction') {
+          return value.length > 160
+            ? { ...prev, [inputName]: { status: 'error', message: '不可超過160字', value } }
+            : { ...prev, [inputName]: { status: 'focus', message: `${value?.length}/160`, value } };
+        }
+      });
+    },
+    handleOnFocus: (inputName, value) => {
+      setInputInfo(prev => {
+        return inputName === 'name'
+          ? { ...prev, [inputName]: { status: 'focus', message: `${value?.length}/50`, value } }
+          : { ...prev, [inputName]: { status: 'focus', message: `${value?.length}/160`, value } };
+      });
+    },
+    handleOnBlur: (inputName, value) => {
+      setInputInfo(prev => {
+        if (inputName === 'name')
+          return !value?.trim()
+            ? { ...prev, [inputName]: { status: 'error', message: '名稱不可為空白', value } }
+            : { ...prev, [inputName]: { status: 'default', message: `${value?.length}/50`, value } };
+        return { ...prev, [inputName]: { status: 'default', message: `${value?.length}/160`, value } };
+      });
+    }
+  };
+
+  return (
+    <div className={style.backDrop}>
+      <div className={style.card}>
+        <div className={style.header}>
+          <Cross className={style.closeBtn} onClick={handleClose} />
+          <h5 className={style.title}>編輯個人資料</h5>
+          <button className={style.submitBtn} onClick={e => handleSubmit(id, e)}>
+            儲存
+          </button>
+        </div>
+        <div className={style.images}>
+          <label htmlFor='cover' className={style.cover}>
+            <input type='file' name='cover' id='cover' onChange={e => handleImageChange(e, setImagePrev, setImageFile)} />
+            <div className={style.coverBackDrop}></div>
+            <img src={imagePrev.cover} onError={e => (e.target.src = 'https://i.imgur.com/vzIPCvD.png')} alt='' />
+            <CoverEdit className={style.editIcon} />
+          </label>
+          <label htmlFor='avatar' className={style.avatar}>
+            <input type='file' name='avatar' id='avatar' onChange={e => handleImageChange(e, setImagePrev, setImageFile)} />
+            <div className={style.avatarBackDrop}></div>
+            <img src={imagePrev.avatar} onError={e => (e.target.src = 'https://i.imgur.com/QljR8Ap.png')} alt='' />
+            <div className={style.ring}></div>
+            <AvatarEdit className={style.editIcon} />
+          </label>
+        </div>
+        <div className={style.inputs}>
+          <Input name={'name'} label={'名稱'} placeholder={'輸入你的暱稱'} status={inputInfo.name.status} message={inputInfo.name.message} value={inputInfo.name.value} onHandlers={onHandlers} />
+          <Input
+            name={'introduction'}
+            label={'自我介紹'}
+            type={'textarea'}
+            status={inputInfo.introduction.status}
+            message={inputInfo.introduction.message}
+            value={inputInfo.introduction.value}
+            onHandlers={onHandlers}
+          />
+        </div>
+        {updateResult && (
+          <div className={style.notification}>
+            <div className={style.notiBackdrop}></div>
+            <div className={style.notiContent}>
+              <NotificationCard status={updateResult.status} message={updateResult.message} />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
