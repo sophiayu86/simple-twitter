7d25fdde439f42c4093973140b75c7097a1a04c8
Roan <119272359+evojroan@users.noreply.github.com>
2023-03-26 20:25:27 +0800

    Feat: AdminUserPage API connection. Comp AdminUserCard redners according to backend data. (#42)

--- src/Components/AdminUserCard/index.js
+++ src/Components/AdminUserCard/index.js
@@ -3,48 +3,42 @@ import styles from "./style.module.css";
 import { ReactComponent as Reply } from "../../Assets/icon/reply.svg";
 import { ReactComponent as Like } from "../../Assets/icon/like.svg";
 
-const AdminUserCard = ({
-  username,
-  account,
-  bgImg,
-  avatar,
-  tweets,
-  likes,
-  following,
-  follower,
-}) => {
+const AdminUserCard = ({ userData }) => {
   return (
     <div className={styles.AdminUserCard}>
-      <div className={styles.bgImg}>{bgImg}背景圖片</div>
-      <div className={styles.nameAndPhoto}>
-        <div className={styles.avatar}>{avatar}大頭照</div>
-        <div className={styles.nameAndId}>
-          <span className={styles.username}>
-            {username}
-            Jane Cathy
-          </span>
-          <span className={styles.account}>{account}@cath1999</span>
-        </div>
+      <div className={styles.cover}>
+        <img src={userData.cover} className={styles.coverImg} alt="coverImg" />
       </div>
+      {
+        <div className={styles.nameAndPhoto}>
+          <div className={styles.avatar}>
+            <img src={userData.avatar} className={styles.avatarImg} alt="avatarImg" />
+          </div>
+          <div className={styles.nameAndId}>
+            <span className={styles.name}>{userData.name}</span>
+            <span className={styles.account}>{userData.account}</span>
+          </div>
+        </div>
+      }
       <div className={styles.rowAction}>
         <div className={styles.reply}>
           <Reply />
-          <div className={styles.actionNumber}>{tweets}1.5K</div>
+          <div className={styles.actionNumber}>{userData.tweets}</div>
         </div>
         <div className={styles.like}>
           <Like />
-          <div className={styles.actionNumber}>{likes}20K</div>
+          <div className={styles.actionNumber}>{userData.likes}</div>
         </div>
       </div>
 
       <div className={styles.follow}>
         <div className={styles.follower}>
-          <span className={"numberText"}>{following}34個</span>
-          <span className={"followText"}>跟隨中</span>
+          <span className={styles.numberText}>{userData.followings}個</span>
+          <span className={styles.followText}>跟隨中</span>
         </div>
         <div className={styles.following}>
-          <span className={"numberText"}>{follower}59個</span>
-          <span className={"followText"}>跟隨者</span>
+          <span className={styles.numberText}>{userData.followers}個</span>
+          <span className={styles.followText}>跟隨者</span>
         </div>
       </div>
     </div>
--- src/Pages/AdminUsersPage/index.js
+++ src/Pages/AdminUsersPage/index.js
@@ -1,15 +1,38 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { AdminNav, Header, AdminUserCard } from "../../Components";
 import styles from "./style.module.css";
+import {getAdminUserCard} from "../../API/AdminUserCardAPI.js";
+
 
 const AdminUserPage = () => {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    const getUserDataAsync = async () => {
+      try{
+      const userCard = await getAdminUserCard();
+
+      setUserData(userCard);
+    }catch(error){console.log("資料提取失敗");
+  }};
+  getUserDataAsync();
+  }, []);
+
+
+  const displayAllCards = userData.map((user) => (
+    <AdminUserCard key={user.id} userData={user} />
+  ));
+
   return (
     <div className={styles.AdminMainPage}>
       <AdminNav currentPage="admin_users" />
       <div className={styles.mainContent}>
         <Header text="使用者列表" />
         <div className={styles.contentList}>
-          <AdminUserCard />
+
+          {displayAllCards}
+
         </div>
       </div>
     </div>
