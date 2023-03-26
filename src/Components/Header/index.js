f8625d4902a97e9168616c5861aeec8a8a303f51
Sophia Yu <31957719+sophiayu86@users.noreply.github.com>
2023-03-27 00:44:07 +0800

    fix: headers' navigate back previous page (#46)

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,18 +1,21 @@
 import React from "react";
-import {  Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ReactComponent as Back } from "../../Assets/icon/back.svg";
 import styles from "./style.module.css";
 
 const Header = ({ text }) => {
-  
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate(-1);
+  };
   return (
     <div className={styles.header}>
-      {text !== "首頁" && text !== "帳戶設定" && text !== "推文清單"&& text !== "使用者列表"&& (
-        <Link to="/main">
-          <Back className={styles.icon}/>
-          </Link>
-       
-      )}
+      {text !== "首頁" &&
+        text !== "帳戶設定" &&
+        text !== "推文清單" &&
+        text !== "使用者列表" && (
+          <Back className={styles.icon} onClick={handleBack} />
+        )}
       <span>{text}</span>
     </div>
   );
