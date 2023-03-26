e7a32c163b10ed7c2cd98faeab4e4f19d1787dc9
sophiayu86 <sophiayu86@gmail.com>
2023-03-26 17:08:22 +0800

    feat: refactor profilepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import './App.css';
+import "./App.css";
 // import { LoginInput, RegisterInput, Nav,SideNav } from './Components';
-import { BrowserRouter, Route, Routes} from "react-router-dom";
-import { SignUpPage, LoginPage, MainPage, AdminLoginPage, AdminMainPage, AdminUsersPage, ProfileTweets, ProfileReply, ProfileLikes, UserFollowerPage, UserFollowingPage, SettingPage, ReplyPage, HomePage } from'./Pages';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  SignUpPage,
+  LoginPage,
+  MainPage,
+  AdminLoginPage,
+  AdminMainPage,
+  AdminUsersPage,
+  ProfilePage,
+  UserFollowerPage,
+  UserFollowingPage,
+  SettingPage,
+  ReplyPage,
+  HomePage,
+} from "./Pages";
 
 const basename = process.env.PUBLIC_URL;
 
@@ -9,43 +22,30 @@ export default function App() {
   return (
     <div className="App">
       <BrowserRouter basename={basename}>
-      
-            <Routes>
-              <Route path="signup" element={<SignUpPage />} />
-              <Route path="login" element={<LoginPage />} />
-              <Route path="main" element={<MainPage />} />
-              <Route path="admin_login" element={<AdminLoginPage />} />
-              <Route path="admin_main" element={<AdminMainPage />} />
-              <Route path="admin_users" element={<AdminUsersPage />} />
-              {/* <Route path="user/:UserId/tweet" element={<ProfileTweets />} /> */}
-              <Route path="user" element={<ProfileTweets />} />
-              <Route
-                path="replied_tweets"
-                element={<ProfileReply />}
-              />
-              <Route path="likes" element={<ProfileLikes />} />
-              {/* <Route
+        <Routes>
+          <Route path="signup" element={<SignUpPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="main" element={<MainPage />} />
+          <Route path="admin_login" element={<AdminLoginPage />} />
+          <Route path="admin_main" element={<AdminMainPage />} />
+          <Route path="admin_users" element={<AdminUsersPage />} />
+          {/* <Route path="user/:UserId/tweet" element={<ProfileTweets />} /> */}
+          <Route path="profile" element={<ProfilePage />} />
+          {/* <Route
                 path="user/:UserId/followers"
                 element={<UserFollowerPage />}
               /> */}
-               <Route
-                path="followers"
-                element={<UserFollowerPage />}
-              />
-              <Route
-                path="following"
-                element={<UserFollowingPage />}
-              />
-              <Route path="user/setting" element={<SettingPage />} />
-              {/* <Route path="tweet/:TweetId/replies" element={<ReplyPage />} /> */}
-              <Route path="replies" element={<ReplyPage />} />
-              <Route path="*" element={<HomePage />} />
-            </Routes>
+          <Route path="followers" element={<UserFollowerPage />} />
+          <Route path="following" element={<UserFollowingPage />} />
+          <Route path="user/setting" element={<SettingPage />} />
+          {/* <Route path="tweet/:TweetId/replies" element={<ReplyPage />} /> */}
+          <Route path="replies" element={<ReplyPage />} />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
-
 
 // export default function App() {
 //   return (
@@ -56,7 +56,7 @@ export default function App() {
 //       <SideNav/>
 //       <RegisterInput/>
 //       </BrowserRouter>
-     
+
 //       <p>test</p>
 //     </div>
 //   );
