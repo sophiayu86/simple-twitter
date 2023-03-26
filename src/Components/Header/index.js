81ab0a08ba9c2d36c9b751a990defeea850c0004
sophiayu86 <sophiayu86@gmail.com>
2023-03-26 14:45:21 +0800

    hide scrollbar, fix typo, add followship pages links, modify back button to main instead of previous page

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,18 +1,16 @@
 import React from "react";
-import {  useNavigate } from "react-router-dom";
+import {  Link } from "react-router-dom";
 import { ReactComponent as Back } from "../../Assets/icon/back.svg";
 import styles from "./style.module.css";
 
 const Header = ({ text }) => {
-  const navigate = useNavigate();
-  const handleBack = () => {
-    navigate(-1);
-  };
+  
   return (
     <div className={styles.header}>
       {text !== "首頁" && text !== "帳戶設定" && text !== "推文清單"&& text !== "使用者列表"&& (
-        
-          <Back className={styles.icon} onClick={handleBack}/>
+        <Link to="/main">
+          <Back className={styles.icon}/>
+          </Link>
        
       )}
       <span>{text}</span>
--- src/Components/ProfileHeader/index.js
+++ src/Components/ProfileHeader/index.js
@@ -1,16 +1,15 @@
 import React from "react";
-import {  useNavigate } from "react-router-dom";
+import {  Link } from "react-router-dom";
 import { ReactComponent as Back } from "../../Assets/icon/back.svg";
 import styles from "./style.module.css";
 
 const ProfileHeader = ({ text, num }) => {
-  const navigate = useNavigate();
-  const handleBack = () => {
-    navigate(-1);
-  };
+  
   return (
     <div className={styles.profileheader}>
-      <Back className={styles.icon} onClick={handleBack}/>
+      <Link to="/main">
+      <Back className={styles.icon} />
+      </Link>
       <div>
       <p>{text}</p>
       <span>{num}推文</span>
