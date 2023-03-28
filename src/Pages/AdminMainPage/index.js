21c0daf791dc696262307927d09ee402a2110e13
evojroan <roan6903@gmail.com>
2023-03-29 00:49:11 +0800

    Feat:AdminMainPage gets all tweets. CSS not yet.

--- src/Pages/AdminMainPage/index.js
+++ src/Pages/AdminMainPage/index.js
@@ -1,16 +1,35 @@
-import React from 'react';
 import { AdminNav, Header } from '../../Components';
 import AdminTweetList from '../../Lists/AdminTweetList';
+import React, { useEffect, useState } from 'react';
 import styles from './style.module.css';
+import { getAllTweets } from '../../API/getAllTweets';
 
 const AdminMainPage = () => {
+  const [tweetsData, setTweetsData] = useState([]);
+
+  const [render, setRender] = useState(0);
+  const handleRender = () => {
+    setRender(prev => (prev += 1));
+  };
+  useEffect(() => {
+    const getData = async () => {
+      const res = await getAllTweets();
+      setTweetsData(res.data);
+    };
+    console.log('MainLayout Render', render);
+    getData();
+  }, [render]);
+
   return (
     <div className={styles.AdminMainPage}>
       <AdminNav currentPage='admin_main' />
       <div className={styles.mainContent}>
         <Header text='推文清單' />
         <div className={styles.contentList}>
-          <AdminTweetList />
+          <AdminTweetList
+            handleRender={handleRender}
+            data={tweetsData}
+          />
         </div>
       </div>
     </div>
